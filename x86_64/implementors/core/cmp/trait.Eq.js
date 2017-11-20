91689c4fa95e430e84f47e99cebc5c70236e5e36
Travis CI User <travis@example.org>
2017-11-20 17:15:35 +0000

    Update documentation

---
+++ x86_64/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["stdsimd"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"stdsimd/vendor/struct.CpuidResult.html\" title=\"struct stdsimd::vendor::CpuidResult\">CpuidResult</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
